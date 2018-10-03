e4f63cf57274b99e49e1a5783d75193a3598390a
BassXiaoSen <314121378@qq.com>
2018-10-03 22:52:19 +0800

    引入webpack进行打包并转义语法，使用node-sass编译scss，使用autoprefixer-cli自动加前缀

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,4 +1,8 @@
-export const RECOMMEND_URL = 'http://localhost:4000/'
+export const RECOMMEND_URL = 'https://qq-music-api.now.sh'
 export const TOPLIST_URL = 'https://qq-music-api.now.sh/top'
-export const SEARCH_URL = 'http://localhost:4000/search'
-export const LYRICS_URL = 'http://localhost:4000/lyrics'
+export const SEARCH_URL = 'https://qq-music-api.now.sh/search'
+export const LYRICS_URL = 'https://qq-music-api.now.sh/lyrics'
+
+// export const RECOMMEND_URL = 'http://localhost:4000/'
+// export const SEARCH_URL = 'http://localhost:4000/search'
+// export const LYRICS_URL = 'http://localhost:4000/lyrics'
